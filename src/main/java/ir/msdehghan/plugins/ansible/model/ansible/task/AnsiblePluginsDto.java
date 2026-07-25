9df96909e517f8abb2a8ee56e84c07ecc52eb009
begris <30695534+begris@users.noreply.github.com>
2026-07-26 01:43:57 +0200

    Removed unused imports

--- src/main/java/ir/msdehghan/plugins/ansible/model/ansible/task/AnsiblePluginsDto.java
+++ src/main/java/ir/msdehghan/plugins/ansible/model/ansible/task/AnsiblePluginsDto.java
@@ -3,8 +3,6 @@ package ir.msdehghan.plugins.ansible.model.ansible.task;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import java.util.List;
-import java.util.Map;
-import java.util.Set;
 
 class AnsiblePluginsDto {
 
